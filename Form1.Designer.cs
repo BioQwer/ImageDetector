07f3c794171e246d1d2e86f4e03bc7a0ff75f621
Bioqwer <BioQwer94@gmail.com>
2014-04-07 15:45:32 +0400

    First and last commit
    Анализ файла с изображением и вывод фото или нет

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ImageDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO! Фото";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chart1.BorderSkin.PageColor = System.Drawing.SystemColors.Window;
+            chartArea1.AxisX.Enabled = System.Windows.Forms.DataVisualization.Charting.AxisEnabled.False;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.chart1.Location = new System.Drawing.Point(322, 43);
+            this.chart1.Name = "chart1";
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            series1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Black;
+            series1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            series1.LabelBorderWidth = 4;
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(419, 254);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(319, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Среднее = ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(168, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "GO! Диаграма";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 245);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(285, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "GO!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(753, 323);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Detector";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.DataVisualization.Charting.Utilities;
+
+namespace ImageDetector
+{
+    public partial class Form1 : Form
+    {
+        
+        static int hist_size = 256;
+        double Mean_koef = 0.03;  //< Диаграмма или граф
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Draw_Histogramm_With_Result_in_File(double[] hist,bool isFoto)
+        {
+            chart1.Series[0].Points.Clear();
+            for (int index = 0; index < hist.GetLength(0); index++)
+            {
+                chart1.Series[0].Points.AddXY(index, hist[index]);
+            }
+            chart1.Invalidate();
+            // Calculate Standard Deviation from the Variance
+            //double variance = chart1.DataManipulator.Statistics.Variance("Series1", true);
+            //double dispersion = Math.Sqrt(variance);
+            
+            double mean = chart1.DataManipulator.Statistics.Mean("Series1");
+            label2.Text = "Среднее = " + mean.ToString();
+            string text;
+            text = mean.ToString();
+            if (isFoto == true)
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"E:\Dropbox\Visual Studio 2012\Projects\ImageDetector\Foto.txt", true))
+                {
+                    file.WriteLine(text);
+                }
+            }
+            else
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"E:\Dropbox\Visual Studio 2012\Projects\ImageDetector\Graphics.txt", true))
+                {
+                    file.WriteLine(text);
+                }
+            }
+        }
+
+        private void DrawHistogramm(double[] hist)
+        {
+            chart1.Series[0].Points.Clear();
+            for (int index = 0; index < hist.GetLength(0); index++)
+            {
+                chart1.Series[0].Points.AddXY(index, hist[index]);
+            }
+            chart1.Invalidate();
+            // Calculate Standard Deviation from the Variance
+            //double variance = chart1.DataManipulator.Statistics.Variance("Series1", true);
+            //double dispersion = Math.Sqrt(variance);
+
+            double mean = chart1.DataManipulator.Statistics.Mean("Series1");
+            label2.Text = "Среднее = " + mean.ToString();
+            //if ((ad > Average_derivative_koef) && (mean < Mean_koef))
+            if (mean<Mean_koef)
+            {
+                MessageBox.Show("Это Диаграмма или График");
+            }
+            else
+            {
+                MessageBox.Show("Это Фото");
+            }
+        }
+
+
+        private double[] GetHistogramm(Bitmap image)
+        {
+            double[] result = new double[hist_size];
+            for (int x = 0; x < image.Width; x++)
+                for (int y = 0; y < image.Height; y++)
+                {
+                    int i = (int)(255 * image.GetPixel(x, y).GetBrightness());
+                    result[i]++;
+                }
+
+            double max = result.Max();
+            for (short i = 0; i < hist_size ; i++)
+                result[i] = result[i] / max;
+
+            return result;
+        }
+
+
+        private void button1_Click(object sender, EventArgs q)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                double[] hist;
+                Image image = Image.FromFile(ofd.FileName);
+                hist = GetHistogramm(image as Bitmap);
+                Draw_Histogramm_With_Result_in_File(hist,true);
+                Bitmap bmp = new Bitmap(ofd.FileName);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                double[] hist;
+                Image image = Image.FromFile(ofd.FileName);
+                hist = GetHistogramm(image as Bitmap);
+                Draw_Histogramm_With_Result_in_File(hist,false);
+                Bitmap bmp = new Bitmap(ofd.FileName);
+                pictureBox1.Image = bmp;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                double[] hist;
+                Image image = Image.FromFile(ofd.FileName);
+                hist = GetHistogramm(image as Bitmap);
+                Bitmap bmp = new Bitmap(ofd.FileName);
+                pictureBox1.Image = bmp;
+                DrawHistogramm(hist);
+            }
+        }
+
+    }
+}
